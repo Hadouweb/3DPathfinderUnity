25d1868e22b400f6451c649c30cf65231411a540
Nicolas LE BRETON <kengo14@hotmail.fr>
2017-10-14 15:06:28 +0200

    Test

--- Assets/scripts/PathFinder3D.cs
+++ Assets/scripts/PathFinder3D.cs
@@ -2,8 +2,11 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PathFinder3D : MonoBehaviour {
+public class PathFinder3D : MonoBehaviour
+{
 
+	public int lol = 123;
+	
 	// Use this for initialization
 	void Start () {
 		
